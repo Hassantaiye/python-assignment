14c571bc3f4641385efd1e41ccb5386bcefc2885
Hassantaiye <114440007+Hassantaiye@users.noreply.github.com>
2025-07-30 07:53:55 -0700

    Add files via upload

---
+++ new.py
@@ -0,0 +1,11 @@
+print("introduct to python assignment")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+subtraction = num1 - num2
+division = num1 / num2
+multiplication = num1 * num2
+print(f"The sum of {num1} and {num2} is: {sum}")
+print(f"The subtraction of {num2} from {num1} is: {subtraction}")
+print(f"The division of {num1} by {num2} is: {division}")
+print(f"The multiplication of {num1} and {num2} is: {multiplication}")
