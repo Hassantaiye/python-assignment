c1ed9d868c216c63efcdf9fdecb6ef2fc86a12b5
Hassantaiye <114440007+Hassantaiye@users.noreply.github.com>
2025-07-31 09:50:09 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,11 +1,14 @@
-print("introduct to python assignment")
+print("introduction to python assignment")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-sum = num1 + num2
-subtraction = num1 - num2
-division = num1 / num2
-multiplication = num1 * num2
-print(f"The sum of {num1} and {num2} is: {sum}")
-print(f"The subtraction of {num2} from {num1} is: {subtraction}")
-print(f"The division of {num1} by {num2} is: {division}")
-print(f"The multiplication of {num1} and {num2} is: {multiplication}")
+operation= input("enter operation (+-/*):")
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '/':
+    result = num1 / num2
+elif operation == '*':
+    result = num1 * num2
+
+print(f"The result of {num1} {operation} {num2} is {result}") 
